f8db616782b6ebc1b516a5519d0b6636c9c73c56
Arjun Rao <arjun.rao@stonybrook.edu>
2017-12-06 18:33:16 -0500

    whoops don't want it to break for y'all

--- WolfieShop/reset_mysql.sql
+++ WolfieShop/reset_mysql.sql
@@ -1,6 +1,6 @@
 
-#GRANT ALL PRIVILEGES ON wolfieshop_db.* TO ''@'localhost';
-GRANT ALL PRIVILEGES ON wolfieshop_db.* TO 'root'@'localhost';#added this for me until i can figure out '' user
+GRANT ALL PRIVILEGES ON wolfieshop_db.* TO ''@'localhost';
+#GRANT ALL PRIVILEGES ON wolfieshop_db.* TO 'root'@'localhost';#added this for me until i can figure out '' user
 FLUSH PRIVILEGES;
 DROP DATABASE wolfieshop_db;
 DROP USER 'lal'@'localhost';
